60ba3699d7ae1e94b3ec86de76b61855e822c58d
Vichoko <vicenteoyanedel@gmail.com>
2016-04-29 10:49:38 -0300

    Restructure Data folder and script polishing

---
+++ Data/jsonrequest.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import time
+
+
+class Request:
+    def __init__(self, address, outfile, key):
+        self.req = requests.get(address + key)
+        self.status = self.req.status_code
+        self.sleepTime = 60
+
+        # Interpret the response received in the request
+        # Successful request
+        if self.status == 200:
+            print("pass")
+            with open(outfile, "w+") as outfile:
+                json.dump(self.req.json(), outfile)
+
+        # Diferent errors are displayed in console.
+        elif self.status == 429:
+            print ("Rate Limit exceded")
+            #Wait some time until continue
+            time.sleep(self.sleepTime)
+
+        elif self.status == 500:
+            print ("Internal server error")
+
+        elif self.status == 503:
+            print ("Service Unavailable")
+
+        elif self.status == 404:
+            print ("Data not found")
+
+        else:
+            print ("ERROR FATAL BRODER " + self.status)
+
