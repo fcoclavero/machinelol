0c820f23a9dfeb10cce94b43aa3477bc7dacda1c
fcoclavero <fcoclavero32@gmail.com>
2016-06-15 12:06:21 -0400

    Created SNN class to manage SNN clusters as if with a library.

--- Clustering/validation.py
+++ Clustering/validation.py
@@ -1,39 +1,5 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import snn
-
-from numpy import genfromtxt
-from sklearn.preprocessing import StandardScaler
-from matplotlib.pyplot import cm
-from mpl_toolkits.mplot3d import Axes3D
-
-def loadData(dataDirectory):
-    # Read data from csv file
-    data = pd.read_csv(dataDirectory + "/csv/Summary/Challenger.csv", sep = ";")
-
-    # Parse data, drops id column
-    X = (data.drop('id', 1)).as_matrix()
-    return data, StandardScaler().fit_transform(X)
-
-# Result visualization
-def plotResults(data, X, jpClusters, nClusters):
-    n = X.shape[0]
-    headers = data.columns.values.tolist()
-
-    color = cm.rainbow(np.linspace(0, 1, nClusters))
-
-    fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    ax = fig.add_subplot(111)
-
-    # Plot data points
-    for i in range(n):
-        col = 'k' if jpClusters[i] == 0 else color[jpClusters[i]]
-        # ax.scatter(X[i][0], X[i][1], X[i][2], 'o', c=col)
-        ax.scatter(X[i][0], X[i][1], c=col)
-
-    plt.show()
+from snnClass import SNN
 
 # Test constants
 k = 25
@@ -43,11 +9,10 @@ min_pts = 10
 dataDirectory = "C:/Users/fcocl_000/Dropbox/Workspace/Python/Aprendizaje Bayesiano/machinelol/Data"
 
 # Execute test
-data, X = loadData(dataDirectory)
-jpClusters, nClusters = snn.snnCluster(X, k, eps, min_pts)
-plotResults(data, X, jpClusters, nClusters)
+data = pd.read_csv(dataDirectory + "/csv/Summary/Challenger.csv", sep = ";")
+
+snn = SNN(data, k, eps, min_pts)
+snn.plot2D()
 
 # Create statistics
-
-
-print("Number of clusters: " + str(nClusters))
+print("Number of clusters: " + str(snn.nClusters))
