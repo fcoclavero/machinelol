19c2fce914e6d0dd8c5c7425d857e41fe8a432e0
fcoclavero <fcoclavero32@gmail.com>
2016-06-23 19:32:36 -0400

    Dummy results page

--- Django/recomendation/views.py
+++ Django/recomendation/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world!")
+
+def results(request, playerId):
+    return HttpResponse("Player id: " + str(playerId))
