09cae13d6f21accaf32e9b4f6610b1709ee73bb1
fcoclavero <fcoclavero32@gmail.com>
2016-07-01 14:30:02 -0400

    Added new homepage app. Included basic bootstrap html template.

--- Django/machinelol/settings.py
+++ Django/machinelol/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
     'recomendation.apps.RecomendationConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- Django/machinelol/urls.py
+++ Django/machinelol/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('homepage.urls')),
     url(r'^recomendation/', include('recomendation.urls')),
     url(r'^admin/', admin.site.urls),
 ]
