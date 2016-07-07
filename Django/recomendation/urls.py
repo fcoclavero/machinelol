a581cbf2be7929d7b5bbc339d8fdb0f236407e65
fcoclavero <fcoclavero32@gmail.com>
2016-07-06 22:14:32 -0400

    Added global static files dir. Fixed some internal links in homepage template.

--- Django/recomendation/urls.py
+++ Django/recomendation/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<playerId>[0-9]+)/$', views.results, name='results'),
+    url(r'^$', views.index, name='recomendation_index'),
+    url(r'^(?P<playerId>[0-9]+)/$', views.results, name='recomendation'),
 ]
