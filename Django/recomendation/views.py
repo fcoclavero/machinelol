14e267e0d18e50a4bdc520ace777cdf1987daac1
fcoclavero <fcoclavero32@gmail.com>
2016-07-08 13:25:59 -0400

    Recomendation page now gets summoner id from posted summoner name and region.

--- Django/recomendation/views.py
+++ Django/recomendation/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponseRedirect
 from .models import User
 from .forms import SignInForm
 
+import requests, json
+
+keys = ["9b25bbda-7da3-4ee5-9d6b-a7ff2c402c0d", "0b808dbd-c044-43db-88a0-829dbd390aa7"]
+
+def getId(summonerName, region):
+    data = requests.get("https://las.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + keys[0])
+
+    # TODO: manage name not found
+    return data.json()[summonerName]['id']
+
 # Create your views here.
 def recomendation(request):
     if request.method == 'POST':
@@ -13,10 +23,10 @@ def recomendation(request):
         # Check validity
         if form.is_valid():
             data = form.cleaned_data
-            return render(request, 'recomendation/index.html', {'summonerName':data['summonerName'], 'region':data['region']})
+            return render(request, 'recomendation/index.html', {'summonerName':data['summonerName'], 'region':data['region'], 'id':getId(data["summonerName"], data["region"])})
 
         # If invalid redirect to error page
         else:
-            return render(request, 'recomendation/error.html', {'message':request.POST})
+            return render(request, 'recomendation/error.html', {'message':"invalid form"})
     else:
         return render(request, 'recomendation/error.html', {'message':"request not post"})
