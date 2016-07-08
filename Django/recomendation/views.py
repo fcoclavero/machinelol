881b079cd9a12027dad034503f2d12b30d454cec
fcoclavero <fcoclavero32@gmail.com>
2016-07-08 17:40:59 -0400

    Moved get data scripts to utilities.py in recomendations app.

--- Django/recomendation/views.py
+++ Django/recomendation/views.py
@@ -4,15 +4,7 @@ from django.http import HttpResponseRedirect
 from .models import User
 from .forms import SignInForm
 
-import requests, json
-
-keys = ["9b25bbda-7da3-4ee5-9d6b-a7ff2c402c0d", "0b808dbd-c044-43db-88a0-829dbd390aa7"]
-
-def getId(summonerName, region):
-    data = requests.get("https://las.api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + keys[0])
-
-    # TODO: manage name not found
-    return data.json()[summonerName]['id']
+from recomendation.utilities import *
 
 # Create your views here.
 def recomendation(request):
