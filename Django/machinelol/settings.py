4faf0c70ae02a6a9685be4590f59b85a4227fbd8
fcoclavero <fcoclavero32@gmail.com>
2016-07-07 20:18:35 -0400

    Sut

--- Django/machinelol/settings.py
+++ Django/machinelol/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'machinelol.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'machinelol_db',
+        'USER': 'root',
+        'PASSWORD': 's1u2t3',
+        'HOST': 'localhost',
+        'PORT': 3307,
     }
 }
 
