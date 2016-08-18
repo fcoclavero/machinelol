b414a3b73abacbcdd9e9c29239c88d724e897c24
fcoclavero <fcoclavero32@gmail.com>
2016-08-18 13:55:47 -0300

    Initial loading dialog implementation.

--- Django/recomendation/urls.py
+++ Django/recomendation/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.recomendation, name='recomendation'),
-    # url(r'^(?P<playerId>[0-9]+)/$', views.recomendation, name='recomendation'), # results via get
+    url(r'^(?P<playerId>[0-9]+)&(?P<region>[0-9]+)/$', views.getRecomendation, name='getRecomendation'), # results via get
 ]
