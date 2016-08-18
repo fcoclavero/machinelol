a515dcb555966f75ef3cd76b6df6446a8dc78736
fcoclavero <fcoclavero32@gmail.com>
2016-08-18 17:57:20 -0300

    Updated ajax call. CSRF cookie succesfully retrieved.

--- Django/recomendation/urls.py
+++ Django/recomendation/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.recomendation, name='recomendation'),
-    url(r'^(?P<playerId>[0-9]+)&(?P<region>[0-9]+)/$', views.getRecomendation, name='getRecomendation'), # results via get
+    url(r'^ajax/$', views.getRecomendation, name='ajax'), # results via get
 ]
